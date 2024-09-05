356e4c68f4ef8ef433e3a741261237944f663c68
Hate Love <shchetinin-02@list.ru>
2024-09-05 19:19:03 +0300

    base logic

---
+++ src/components/Board.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Card from "./Card";
+
+
+
+ 
+
+
+export default function Board() {
+   
+    return (
+        
+            <Card/> 
+
+
+        
+    );
+}
